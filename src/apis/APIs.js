cf97df4db578e43a56ea912d1840b71b2b425978
youyeul lee <a1s1a1@naver.com>
2023-09-13 14:38:09 +0900

    파일업로드 유효성 검사 로직

--- src/apis/APIs.js
+++ src/apis/APIs.js
@@ -1,6 +1,37 @@
-export function test(test){                    
+// export function test(test){                    
+//     return new Promise((resolve,reject)=>{
+//         <>
+//         </>
+//     })  
+// }
+
+export function excelUpload(json){
     return new Promise((resolve,reject)=>{
-        <>
-        </>
-    })  
+        fetch('http://localhost:8080/revenue', {
+            method: "POST",
+            headers: {'Content-type': 'application/json'},
+            body: json,
+        })
+        .then((result)=>{
+            console.log(result);
+            console.log(result.status);
+            if(result.status === 201){
+                console.log('저장완료');
+            }else{
+                console.log('저장실패');
+            }
+        }) 
+    })
+}
+
+export function inquireExcelByDay(json){
+    return new Promise((resolve,reject)=>{
+        fetch('http://localhost:8080/revenueDay', {
+            method: "GET",
+        })
+        .then(res=> res.json())
+        .then(json => {
+            resolve(json);
+        })
+    })
 }
--- src/components/uploadExcel/ExcelUpload.js
+++ src/components/uploadExcel/ExcelUpload.js
@@ -2,50 +2,28 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import * as XLSX from 'xlsx'
 import axios from 'axios'
+import { excelUpload } from '../../apis/APIs';
 const Upload = () =>{ 
     const [fileName, setFileName] = useState(null);
 
-    const handleFile = async (e)=> {
-        const file = e.target.files[0];
-        const data = await file.arrayBuffer();
-        const workbook = XLSX.readFile(data);
-        setFileName(file.name);
-        const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-        const jsonData = XLSX.utils.sheet_to_json(worksheet);
-        // console.log(jsonData);
-        // convert(jsonData);
-        const json = JSON.stringify(jsonData);
-        console.log(jsonData);
-        axios.post('http://localhost:8080/revenue', {
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: json
-        })
-        .then((result)=>{
-            if(result.data.Status === 'Invalid'){
-                alert('Invalid User');
-            }else{
-                // history to push dashboard
-                // history(`/Login`);
-            }
-        })
+
+    const handleFile = async (e)=>{
+
+        if(e.target.value.substr(e.target.value.length-4) === 'xlsx' || e.target.value.substr(e.target.value.length-3) === 'xls'){
+            const file = e.target.files[0];
+            
+            const data = await file.arrayBuffer();
+            const workbook = XLSX.readFile(data);
+            setFileName(file.name);
+            const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+            const jsonData = XLSX.utils.sheet_to_json(worksheet);
+            const json = JSON.stringify(jsonData);
+            await excelUpload(json);
+        }else{
+            alert("맞지않는 파일 형식입니다")
+        }
+        
     }
-
-    // const convert = (inputArray)=>{
-    //     var outPutArray = [];
-    //     inputArray.map(item => {
-    //         let childs = outPutArray[item["Parent"]-1] ? outPutArray[item["Parent"]-1]["Child"] : [] 
-    //         childs.push(item["Child"])
-    //         outPutArray[item["Parent"]-1] = outPutArray[item["Parent"]-1] ? outPutArray[item["Parent"]-1] :{}
-    //         outPutArray[item["Parent"]-1]["Parent"] = item["Parent"]
-    //         outPutArray[item["Parent"]-1]["Child"] = childs
-    //     })
-    //     const json = JSON.stringify(outPutArray);
-    //     document.getElementById('json-result').innerHTML = json;
-    //     console.log(json,"dwqdwq")
-
-    // }
 
     return(
         <div>
@@ -58,10 +36,10 @@ const Upload = () =>{
                                 <p>
                                     FileName: <span>{fileName}</span>
                                 </p>
-                            )
+                                )
 
                             }
-                            <input id="file_upload" type="file" className="form-control-image pd-image-upload-file float-left" onChange={(e) => handleFile(e)} name="xl-docs" />
+                            <input name="fileInput" id="file_upload" type="file" accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel" className="form-control-image pd-image-upload-file float-left" onChange={(e) => handleFile(e)} name="xl-docs" />
                         </div>
                     </div>
                     <div className="col-md-6 m-2">
