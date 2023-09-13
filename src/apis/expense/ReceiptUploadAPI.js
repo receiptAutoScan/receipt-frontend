0868d7fc4fc9c57f89975be27cfb3ccfaaf2b9fc
ej20001000 <ejayyj1225@gmail.com>
2023-09-13 18:36:27 +0900

    Feat: 이미지 업로드 서버 연결 로직 구현
    resolves: #17

--- src/apis/expense/ReceiptUploadAPI.js
+++ src/apis/expense/ReceiptUploadAPI.js
@@ -1,8 +1,15 @@
-// import { resolve } from "path";
+import axios from 'axios';
 
-
-// export function uploadReceiptImage() {
-//     return new Promise((resolve, reject) => {
-//        fetch("http://localhost:8080/exp") 
-//     });
-// }
+export async function uploadReceiptImage(files) {
+    const url = "http://localhost:8080/expense";
+    try {
+        await axios.post(url, files, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        });
+    } catch (error) {
+        console.error(error);
+        alert("영수증 업로드에 실패했습니다.")
+    }
+}
