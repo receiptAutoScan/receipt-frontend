d2901cd360ce57052eb3b54f6e6a30faa66e2f28
ej20001000 <ejayyj1225@gmail.com>
2023-09-26 14:39:32 +0900

    조회 살짝 건드림

--- src/components/SearchAll.js
+++ src/components/SearchAll.js
@@ -7,7 +7,7 @@ import GetMonthlyList from "./commons/GetMonthlyList";
 import ProfitList from "./profit/ProfitList";
 
 function SearchAll(){
-    const [stateMenu, setStateMenu] = useState(1);
+    const [stateMenu, setStateMenu] = useState(3);
     const [dateScope, setDateScope] = useState(1); // 1 : 일별, 2 : 월별
 
     const onClickHandler = (e) => {
@@ -17,8 +17,8 @@ function SearchAll(){
         });
 
         switch(e.target.textContent){
-            case "매출" : setStateMenu(1);e.target.className+=" bgb"; break;
-            case "매입" : setStateMenu(2);e.target.className+=" bgb"; break;
+            case "지출" : setStateMenu(1);e.target.className+=" bgb"; break;
+            case "매출" : setStateMenu(2);e.target.className+=" bgb"; break;
             case "수익" : setStateMenu(3);e.target.className+=" bgb"; break;
             default : break;
         }
@@ -35,7 +35,7 @@ function SearchAll(){
             <div className="df pcc pic w100 jcc h150p">
                 <div className="df w10 pcc jcsb">
                     <div onClick={onClickHandler} className="w50p h50p df pcc pic b1sb hc tbg">매출</div>
-                    <div onClick={onClickHandler} className="w50p h50p df pcc pic b1sb hc tbg">매입</div>
+                    <div onClick={onClickHandler} className="w50p h50p df pcc pic b1sb hc tbg">지출</div>
                     <div onClick={onClickHandler} className="w50p h50p df pcc pic b1sb hc tbg">수익</div>
                 </div>
                 { stateMenu !== 3 &&
